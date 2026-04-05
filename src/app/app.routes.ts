7954e9aee93ab37d7331eeabe54283afae5bd42e
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2026-04-05 11:03:48 +0530

    4th april

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,23 @@
 import { Routes } from '@angular/router';
 import { Products } from './products/products';
 import { AboutUs } from './about-us/about-us';
-
+import { AddProduct } from './add-product/add-product';
+import { NotFound } from './not-found/not-found';
+//adslkjfksajdf
 export const routes: Routes = [
     {
-        path: '', component: Products 
+        path: '', component: Products ,
+    
     },
     {
-        path: 'about-us', component: AboutUs //about-us
+        path: 'about-us', component: AboutUs 
     },
+    {
+        path: 'add-product', component: AddProduct 
+    },
+    {
+        path:'**', component: NotFound  
+    }
 ];
 
 
